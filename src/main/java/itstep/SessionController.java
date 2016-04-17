fdf075d25fbae6d65ee1b62690ac934f196f81a0
Vadim Itstep <root@test.ru>
2016-04-17 10:28:00 +0300

    add user

--- src/main/java/itstep/SessionController.java
+++ src/main/java/itstep/SessionController.java
@@ -14,5 +14,23 @@ public class SessionController {
     public String showSession(HttpSession httpSession){
         return httpSession.getId();
     }
-   
+    @RequestMapping("/read")
+    public String read(HttpSession httpSession){
+        
+        return httpSession.getAttribute("hello").toString();
+    } 
+    @RequestMapping("/write")
+    public String write(HttpSession httpSession, String message){
+      httpSession.setAttribute("hello", message);
+      return message; 
+    }
+    @RequestMapping("/wr")
+    public String wr(HttpSession httpSession, String message, String attribute){
+      httpSession.setAttribute(attribute, message);
+      return message; 
+    }
+    @RequestMapping("/rd")
+    public String rd(HttpSession httpSession, String attribute){
+        return httpSession.getAttribute(attribute).toString();
+    }
 }
